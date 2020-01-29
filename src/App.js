071fd58ec3b0dfe77694e2e86c7c04f4dae1149d
Homero Silva <hsilva@accuray.com>
2020-01-29 10:13:07 -0600

    finishing add modal, todo: use context

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import { Fab, Icon } from '@material-ui/core';
+
 
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      
+      <Header />
+      <Fab style={{backgroundColor: '#1976d2', position:'absolute', right:'30px', bottom:'15px'}} color='primary' aria-label='add'>
+        <Icon>
+          addicon
+        </Icon>
+      </Fab> 
     </div>
   );
 }
--- src/components/NewTask.js
+++ src/components/NewTask.js
@@ -2,46 +2,85 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 import Backdrop from '@material-ui/core/Backdrop';
+import Button from '@material-ui/core/Button';
 import Fade from '@material-ui/core/Fade';
+import TextField from '@material-ui/core/TextField';
+import { DropzoneArea } from 'material-ui-dropzone'
 
 const useStyles = makeStyles(theme => ({
-  modal: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  paper: {
-    backgroundColor: theme.palette.background.paper,
-    border: '2px solid #000',
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-  },
+    modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    paper: {
+        backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+    },
+    form: {
+        display: 'grid',
+        gridGap: '10px'
+    },
+    footer:{
+        display: 'flex',
+        justifyContent: 'flex-end'
+    }
 }));
 
-export default function NewTask({open, setOpen}) {
-  const classes = useStyles();
+export default function NewTask({ open, setOpen }) {
+    const classes = useStyles();
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+    const handleClose = () => {
+        setOpen(false);
+    };
 
-  return (
-      <Modal
-        className={classes.modal}
-        open={open}
-        onClose={handleClose}
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-        BackdropProps={{
-          timeout: 500,
-        }}
-      >
-        <Fade in={open}>
-          <div className={classes.paper}>
-            <h2 id="transition-modal-title">New Task</h2>
-            <p id="transition-modal-description">Add stuff</p>
-          </div>
-        </Fade>
-      </Modal>
-  );
+    return (
+        <Modal
+            className={classes.modal}
+            open={open}
+            onClose={handleClose}
+            closeAfterTransition
+            BackdropComponent={Backdrop}
+            BackdropProps={{
+                timeout: 500,
+            }}
+        >
+            <Fade in={open}>
+                <div className={classes.paper}>
+                    <h2 id="transition-modal-title">New Task</h2>
+
+                    <form className={classes.form}>
+                        <TextField
+                            label="Title" />
+                        <TextField
+                            label="Description"
+                            multiline
+                            rows="4"
+                            rowsMax="8"
+                        />
+                        <TextField
+                            id="datetime-local"
+                            label="Due date"
+                            type="datetime-local"
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                        />
+                        <DropzoneArea
+                            useChipsForPreview={true}
+                            onChange={() => console.log('kek')}
+                        />
+
+                        <div className={classes.footer}>
+                            <Button variant="contained" color="primary">
+                                Save
+                            </Button>
+                        </div>
+                    </form>
+                </div>
+            </Fade>
+        </Modal>
+    );
 }
