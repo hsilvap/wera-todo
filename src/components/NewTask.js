e4ce63c6e99b52738bdbb6a0626adbf61be92e06
H <hsilvap@me.com>
2022-04-18 19:12:47 -0500

    Doing requests

--- src/components/NewTask.js
+++ src/components/NewTask.js
@@ -1,220 +1,232 @@
-import React, { useContext, useState } from 'react';
-import { StoreContext } from '../context/store';
-import { StoreActions } from '../context/reducer';
-import { makeStyles } from '@material-ui/core/styles';
-import Modal from '@material-ui/core/Modal';
-import Backdrop from '@material-ui/core/Backdrop';
-import Button from '@material-ui/core/Button';
-import Fade from '@material-ui/core/Fade';
-import TextField from '@material-ui/core/TextField';
+import React, { useContext, useState } from 'react'
+import { StoreContext } from '../context/store'
+import { StoreActions } from '../context/reducer'
+import { makeStyles } from '@material-ui/core/styles'
+import Modal from '@material-ui/core/Modal'
+import Backdrop from '@material-ui/core/Backdrop'
+import Button from '@material-ui/core/Button'
+import Fade from '@material-ui/core/Fade'
+import TextField from '@material-ui/core/TextField'
 import { DropzoneArea } from 'material-ui-dropzone'
-import { Slider, Typography } from '@material-ui/core';
+import { Slider, Typography } from '@material-ui/core'
 import TagsInput from 'react-tagsinput'
-import DateTimePicker from 'react-datetime-picker';
 import 'react-tagsinput/react-tagsinput.css'
-import db from '../db';
+import db from '../db'
 
 const useStyles = makeStyles(theme => ({
-    modal: {
-        display: 'flex',
-        flexDirection:'column',
-        alignItems: 'center',
-        overflow:'scroll',
-    },
-    paper: {
-        backgroundColor: theme.palette.background.paper,
-        border: '2px solid #000',
-        boxShadow: theme.shadows[5],
-        padding: theme.spacing(2, 4, 3),
-        maxWidth:'430px'
-    },
-    form: {
-        display: 'grid',
-        gridGap: '10px'
-    },
-    footer: {
-        display: 'flex',
-        justifyContent: 'flex-end'
-    },
-    primaryColor:{
-        borderColor: '#3f51b5'
-    },
-    dropZoneArea:{
-        minHeight: 180,
-        maxWidth: 410
-    },
-    dropZoneAreaText:{
-        fontSize: 18
-    }
-}));
+  modal: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    overflow: 'scroll'
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+    maxWidth: '430px'
+  },
+  form: {
+    display: 'grid',
+    gridGap: '10px'
+  },
+  footer: {
+    display: 'flex',
+    justifyContent: 'flex-end'
+  },
+  primaryColor: {
+    borderColor: '#3f51b5'
+  },
+  dropZoneArea: {
+    minHeight: 100,
+    maxWidth: 410
+  },
+  dropZoneAreaText: {
+    fontSize: 18
+  }
+}))
 
 export const priorities = [
-    {
-        value: 0,
-        label: 'Low',
-      },
-      {
-        value: 1,
-        label: 'Medium',
-      },
-      {
-        value: 2,
-        label: 'High',
-      },
-      {
-        value: 3,
-        label: 'Urgent',
-      },
-      {
-        value: 4,
-        label: 'Critical',
-      },
-];
+  {
+    value: 0,
+    label: 'Low'
+  },
+  {
+    value: 1,
+    label: 'Medium'
+  },
+  {
+    value: 2,
+    label: 'High'
+  },
+  {
+    value: 3,
+    label: 'Urgent'
+  },
+  {
+    value: 4,
+    label: 'Critical'
+  }
+]
 
-const valuetext = ( value ) =>{
-    return priorities[value].label;
+const valuetext = value => {
+  return priorities[value].label
 }
 
-export default function NewTask() {
-    const baseTask  = {
-        title:"",
-        description:"",
-        dueDate:"",
-        priority:1,
-        links:[],
-        files:[],
-        complete: false,
+export default function NewTask () {
+  const baseTask = {
+    title: '',
+    description: '',
+    dueDate: '',
+    priority: 1,
+    links: [],
+    files: [],
+    complete: false
+  }
+  const classes = useStyles()
+  const { state, dispatch } = useContext(StoreContext)
+  const [task, setTask] = useState(baseTask)
+  const [saving, setsaving] = useState(false)
+
+  const handleClose = () => {
+    setTask(baseTask)
+    dispatch({ type: StoreActions.CANCEL_CREATE_NEW })
+  }
+
+  const handleSave = async () => {
+    setsaving(true)
+    const filesUrls = []
+
+    for (const file of task.files) {
+      await db
+        .storage()
+        .ref(`${state.user.uid}/${file.name}`)
+        .put(file)
+        .then(function (fileSnapshot) {
+          return fileSnapshot.ref
+            .getDownloadURL()
+            .then(url => filesUrls.push(url))
+        })
     }
-    const classes = useStyles();
-    const { state, dispatch } = useContext(StoreContext);
-    const [task, setTask] = useState(baseTask);
-    const [saving, setsaving] = useState(false)
-    
-    const handleClose = () => {
-        setTask(baseTask)
-        dispatch({ type: StoreActions.CANCEL_CREATE_NEW })
-    };
+    var data = { ...task }
+    data.files = [...filesUrls]
+    await db
+      .firestore()
+      .collection('tasks')
+      .doc(state.user.uid)
+      .collection('todo')
+      .add(data)
+    dispatch({
+      type: StoreActions.SHOW_NOTIFICATION,
+      data: { notification: 'New task created!' }
+    })
+    setsaving(false)
+    handleClose()
+  }
 
-    const handleSave = async () =>{
-        setsaving(true)
-        const filesUrls = []
+  const handleFileChange = async uploadedFile => {
+    setTask({ ...task, files: [...task.files, uploadedFile] })
+  }
 
-        for (const file of task.files) {
-            await db.storage().ref(`${state.user.uid}/${file.name}`).put(file).then(function(fileSnapshot){
-                return fileSnapshot.ref.getDownloadURL().then((url) => filesUrls.push(url))
-            })
-        }
-        var data = {...task}
-        data.files = [...filesUrls]
-        await db.firestore().collection('tasks').doc(state.user.uid).collection('todo').add(data)
-        dispatch({type: StoreActions.SHOW_NOTIFICATION, data: { notification : 'New task created!'}})
-        setsaving(false)
-        handleClose();
-    }
+  const handleFileDelete = file => {
+    const list = task.files.filter(x => x !== file)
+    setTask({ ...task, files: list })
+  }
 
-    const handleFileChange = async (uploadedFile) =>{
-        setTask({...task, files: [...task.files, uploadedFile]})
-    }
+  return (
+    <Modal
+      className={classes.modal}
+      open={state.createNew}
+      onClose={handleClose}
+      closeAfterTransition
+      BackdropComponent={Backdrop}
+      BackdropProps={{
+        timeout: 500
+      }}
+    >
+      <Fade in={state.createNew}>
+        <div className={classes.paper}>
+          <h2 id='transition-modal-title'>New Task</h2>
 
-    const handleFileDelete = (file) =>{
-        const list = task.files.filter( x => x !== file)
-        setTask({...task, files: list})
-    }
-    
-    return (
-        <Modal
-            className={classes.modal}
-            open={state.createNew}
-            onClose={handleClose}
-            closeAfterTransition
-            BackdropComponent={Backdrop}
-            BackdropProps={{
-                timeout: 500,
-            }}
-        >
-            <Fade in={state.createNew}>
-                <div className={classes.paper}>
-                    <h2 id="transition-modal-title">New Task</h2>
+          <form className={classes.form}>
+            <TextField
+              required
+              label='Title'
+              value={task.title}
+              onChange={e => setTask({ ...task, title: e.target.value })}
+            />
+            <TextField
+              label='Description'
+              multiline
+              rows='2'
+              rowsMax='4'
+              value={task.description}
+              onChange={e => setTask({ ...task, description: e.target.value })}
+            />
 
-                    <form className={classes.form}>
-                        <TextField
-                            required
-                            label="Title"
-                            value={task.title}
-                            onChange={(e)=>setTask({...task, title:e.target.value})}
-                            />
-                        <TextField
-                            label="Description"
-                            multiline
-                            rows="2"
-                            rowsMax="4"
-                            value={task.description}
-                            onChange={(e)=>setTask({...task, description:e.target.value})}
+            <TextField
+              required
+              label='Due Date'
+              type='datetime-local'
+              value={task.dueDate}
+              onChange={e => {
+                setTask({ ...task, dueDate: e.target.value })
+              }}
+              InputLabelProps={{
+                shrink: true
+              }}
+            />
 
-                        />
-
-                        <>
-                        <Typography   gutterBottom>
-                            Due date
-                        </Typography>
-                        <DateTimePicker
-                            value={task.dueDate}
-                            disableClock
-                            onChange={(date)=>{ setTask({...task, dueDate:date})}}
-                            minDate={new Date()}
-                        />
-                        </>
-
-                        <>
-                        <Typography  gutterBottom>
-                            Priority
-                        </Typography>
-                        <Slider
-                         defaultValue={1}
-                         aria-labelledby="discrete-slider"
-                         valueLabelDisplay="auto"
-                         valueLabelFormat={valuetext}
-                         step={1}
-                         marks={priorities}
-                         min={0}
-                         max={4}
-                         value={task.priority}
-                         onChange={(e,newVal) => setTask({...task, priority: newVal})}
-                        />
-                        </>
-                        <>
-                        <Typography  gutterBottom>
-                            Links
-                        </Typography>
-                        <TagsInput 
-                        focusedClassName={classes.primaryColor}
-                        inputProps={{placeholder: 'Add'}}
-                        value={task.links}
-                        onlyUnique
-                        onChange={(link)=> setTask({...task, links: [...link]})} 
-                        />
-                        </>
-                        <>
-                        <Typography  gutterBottom>
-                            Files
-                        </Typography>
-                        <DropzoneArea
-                            useChipsForPreview={true}
-                            onDrop={(e) => handleFileChange(e)}
-                            onDelete={(e) => handleFileDelete(e)}
-                            dropzoneClass={classes.dropZoneArea}
-                            dropzoneParagraphClass={classes.dropZoneAreaText}
-                            dropzoneText={"Drag and drop a file here or click"}
-                         />
-                        </>
-                        <div className={classes.footer}>
-                            <Button variant="contained" color="primary" onClick={handleSave} disabled={saving || !task.dueDate }>
-                                Save
-                            </Button>
-                        </div>
-                    </form>
-                </div>
-            </Fade>
-        </Modal>
-    );
+            <>
+              <Typography gutterBottom>Priority</Typography>
+              <Slider
+                defaultValue={1}
+                aria-labelledby='discrete-slider'
+                valueLabelDisplay='auto'
+                valueLabelFormat={valuetext}
+                step={1}
+                marks={priorities}
+                min={0}
+                max={4}
+                value={task.priority}
+                onChange={(e, newVal) => setTask({ ...task, priority: newVal })}
+              />
+            </>
+            <>
+              <Typography gutterBottom>Links</Typography>
+              <TagsInput
+                focusedClassName={classes.primaryColor}
+                inputProps={{ placeholder: 'Add' }}
+                value={task.links}
+                onlyUnique
+                onChange={link => setTask({ ...task, links: [...link] })}
+              />
+            </>
+            <>
+              <Typography gutterBottom>Files</Typography>
+              <DropzoneArea
+                useChipsForPreview={true}
+                onDrop={e => handleFileChange(e)}
+                onDelete={e => handleFileDelete(e)}
+                dropzoneClass={classes.dropZoneArea}
+                dropzoneParagraphClass={classes.dropZoneAreaText}
+                dropzoneText={'Add a file'}
+              />
+            </>
+            <div className={classes.footer}>
+              <Button
+                variant='contained'
+                color='primary'
+                onClick={handleSave}
+                disabled={saving || !task.dueDate}
+              >
+                Save
+              </Button>
+            </div>
+          </form>
+        </div>
+      </Fade>
+    </Modal>
+  )
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,27 +1,27 @@
-import React, {useEffect, useState , useContext } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
+import React, { useEffect, useState, useContext } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import clsx from 'clsx'
+import Card from '@material-ui/core/Card'
+import CardHeader from '@material-ui/core/CardHeader'
+import CardContent from '@material-ui/core/CardContent'
+import CardActions from '@material-ui/core/CardActions'
+import Collapse from '@material-ui/core/Collapse'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
 import moment from 'moment'
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import { blueGrey } from '@material-ui/core/colors';
-import { red } from '@material-ui/core/colors';
-import EditIcon from '@material-ui/icons/Edit';
-import DoneIcon from '@material-ui/icons/Done';
-import DirectionsRunIcon from '@material-ui/icons/DirectionsRun';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import { StoreActions } from '../context/reducer';
-import { StoreContext } from '../context/store';
+import IconButton from '@material-ui/core/IconButton'
+import Typography from '@material-ui/core/Typography'
+import { blueGrey } from '@material-ui/core/colors'
+import { red } from '@material-ui/core/colors'
+import EditIcon from '@material-ui/icons/Edit'
+import DoneIcon from '@material-ui/icons/Done'
+import DirectionsRunIcon from '@material-ui/icons/DirectionsRun'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import { StoreActions } from '../context/reducer'
+import { StoreContext } from '../context/store'
 
-import db from '../db';
-import PriorityAvatar from './PriorityAvatar';
+import db from '../db'
+import PriorityAvatar from './PriorityAvatar'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -30,124 +30,195 @@ const useStyles = makeStyles(theme => ({
   },
   media: {
     height: 0,
-    paddingTop: '56.25%', // 16:9
+    paddingTop: '56.25%' // 16:9
   },
   expand: {
     transform: 'rotate(0deg)',
     marginLeft: 'auto',
     transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
+      duration: theme.transitions.duration.shortest
+    })
   },
-  breakText:{
+  breakText: {
     whiteSpace: 'nowrap',
     overflow: 'hidden',
-    textOverflow: 'ellipsis',
+    textOverflow: 'ellipsis'
   },
   expandOpen: {
-    transform: 'rotate(180deg)',
+    transform: 'rotate(180deg)'
   },
   expired: {
-    backgroundColor: blueGrey[100],
+    backgroundColor: blueGrey[100]
   },
   expiredLabel: {
-    color: red[500],
-  },
-}));
+    color: red[500]
+  }
+}))
 
-const Task = ({todo, userUid}) => {
-  const classes = useStyles();
-  const { dispatch, state } = useContext(StoreContext);
-  const [expanded, setExpanded] = React.useState(false);
+const Task = ({ todo, userUid }) => {
+  const classes = useStyles()
+  const { dispatch, state } = useContext(StoreContext)
+  const [expanded, setExpanded] = React.useState(false)
   const [downloads, setDownloads] = useState([])
-  const isExpired = moment(todo.dueDate.toDate()) < moment(Date.now())
+  const dateType = typeof todo.dueDate
+  const isExpired =
+    moment(dateType == 'string' ? todo.dueDate : todo.dueDate.toDate()) <
+    moment(Date.now())
+
   const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
+    setExpanded(!expanded)
+  }
 
   useEffect(() => {
     let dl = []
     todo.files.forEach(file => {
-        const name = db.storage().refFromURL(file).name
-        dl.push({name, file})
-    });
-    setDownloads(dl);
+      const name = db.storage().refFromURL(file).name
+      dl.push({ name, file })
+    })
+    setDownloads(dl)
   }, [todo.files])
 
-  const handleDownload = (file) =>{
+  const handleDownload = file => {
     window.open(file)
   }
 
-  const handleEditTask = () =>{
-    dispatch({type: StoreActions.EDIT_TASK, data: { todo, userUid}})
+  const handleEditTask = () => {
+    dispatch({ type: StoreActions.EDIT_TASK, data: { todo, userUid } })
   }
 
-  const handleMarkComplete = (uid) => {
-    db.firestore().collection('tasks').doc(userUid).collection('todo').doc(uid).update({complete:true})
-    dispatch({type: StoreActions.SHOW_NOTIFICATION, data: { notification : 'Task completed!'}})
-
+  const handleMarkComplete = uid => {
+    db.firestore()
+      .collection('tasks')
+      .doc(userUid)
+      .collection('todo')
+      .doc(uid)
+      .update({ complete: true })
+    dispatch({
+      type: StoreActions.SHOW_NOTIFICATION,
+      data: { notification: 'Task completed!' }
+    })
   }
 
-  const handleReOpen = (uid) => {
-    db.firestore().collection('tasks').doc(userUid).collection('todo').doc(uid).update({complete:false})
-    dispatch({type: StoreActions.SHOW_NOTIFICATION, data: { notification : 'Task re-opened!'}})
+  const handleReOpen = uid => {
+    db.firestore()
+      .collection('tasks')
+      .doc(userUid)
+      .collection('todo')
+      .doc(uid)
+      .update({ complete: false })
+    dispatch({
+      type: StoreActions.SHOW_NOTIFICATION,
+      data: { notification: 'Task re-opened!' }
+    })
   }
 
   return (
-    <Card className={clsx(!isExpired ? classes.root : [classes.root, classes.expired])}>
+    <Card
+      className={clsx(
+        !isExpired ? classes.root : [classes.root, classes.expired]
+      )}
+    >
       <CardHeader
-        avatar={
-            <PriorityAvatar priority={todo.priority}/>
-        }
-        classes={{content: classes.breakText}}
+        avatar={<PriorityAvatar priority={todo.priority} />}
+        classes={{ content: classes.breakText }}
         action={
-          !state.showCompleted &&  <IconButton onClick={()=> handleEditTask()} aria-label="settings">
-            <EditIcon />
-          </IconButton>
+          !state.showCompleted && (
+            <IconButton onClick={() => handleEditTask()} aria-label='settings'>
+              <EditIcon />
+            </IconButton>
+          )
         }
         title={todo.title}
-        subheader={<span className={clsx(isExpired ? classes.expiredLabel : '')}> { isExpired ? moment(todo.dueDate.toDate()).format('[EXPIRED - due date] Do MMMM YYYY, h:mm:ss a') :moment(todo.dueDate.toDate()).format('[Complete me before] Do MMMM YYYY, h:mm:ss a')} </span>}
+        subheader={
+          <span className={clsx(isExpired ? classes.expiredLabel : '')}>
+            {' '}
+            {isExpired
+              ? moment(
+                  dateType == 'string' ? todo.dueDate : todo.dueDate.toDate()
+                ).format('[EXPIRED - due date] Do MMMM YYYY, h:mm:ss a')
+              : moment(
+                  dateType == 'string' ? todo.dueDate : todo.dueDate.toDate()
+                ).format('[Complete me before] Do MMMM YYYY, h:mm:ss a')}{' '}
+          </span>
+        }
       />
       <CardContent>
-        <Typography variant="body2" color="textSecondary" title={todo.description} component="p" className={clsx({
-            [classes.breakText]: !expanded,
-          })}>
-            {todo.description}
+        <Typography
+          variant='body2'
+          color='textSecondary'
+          title={todo.description}
+          component='p'
+          className={clsx({
+            [classes.breakText]: !expanded
+          })}
+        >
+          {todo.description}
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
-        { !state.showCompleted ? <IconButton onClick={()=> handleMarkComplete(todo.uid)} color="primary" aria-label="Mark complete" >
-          <DoneIcon />
-        </IconButton>
-        :
-        <IconButton onClick={()=> handleReOpen(todo.uid)} color="primary" aria-label="Re-open" >
-          <DirectionsRunIcon />
-        </IconButton>
-        
-        }
+        {!state.showCompleted ? (
+          <IconButton
+            onClick={() => handleMarkComplete(todo.uid)}
+            color='primary'
+            aria-label='Mark complete'
+          >
+            <DoneIcon />
+          </IconButton>
+        ) : (
+          <IconButton
+            onClick={() => handleReOpen(todo.uid)}
+            color='primary'
+            aria-label='Re-open'
+          >
+            <DirectionsRunIcon />
+          </IconButton>
+        )}
         <IconButton
           className={clsx(classes.expand, {
-            [classes.expandOpen]: expanded,
+            [classes.expandOpen]: expanded
           })}
           onClick={handleExpandClick}
           aria-expanded={expanded}
-          aria-label="show more"
+          aria-label='show more'
         >
           <ExpandMoreIcon />
         </IconButton>
       </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
+      <Collapse in={expanded} timeout='auto' unmountOnExit>
         <CardContent>
           <Typography paragraph>Reference links:</Typography>
-          <List component="nav" aria-label="main mailbox folders">
-          {todo.links.map((link, index) => <ListItem component="a" button target="_blank" rel={"noreferrer"} title={link} classes={{ root: classes.breakText}} href={link} key={`${link} - ${index}`}> {link} </ListItem>)} 
+          <List component='nav' aria-label='main mailbox folders'>
+            {todo.links.map((link, index) => (
+              <ListItem
+                component='a'
+                button
+                target='_blank'
+                rel={'noreferrer'}
+                title={link}
+                classes={{ root: classes.breakText }}
+                href={link}
+                key={`${link} - ${index}`}
+              >
+                {' '}
+                {link}{' '}
+              </ListItem>
+            ))}
           </List>
           <Typography paragraph>Files:</Typography>
-          {downloads.map((file, index) => <ListItem  component="a" button onClick={()=>handleDownload(file.file)} key={`${file} - ${index}`}>{file.name} </ListItem>)} 
+          {downloads.map((file, index) => (
+            <ListItem
+              component='a'
+              button
+              onClick={() => handleDownload(file.file)}
+              key={`${file} - ${index}`}
+            >
+              {file.name}{' '}
+            </ListItem>
+          ))}
         </CardContent>
       </Collapse>
     </Card>
-  );
+  )
 }
 
 export default Task
