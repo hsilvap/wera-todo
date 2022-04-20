f952507a764d4a504d833090634409e76e6d5d44
H <hsilvap@me.com>
2022-04-20 16:52:51 -0500

    add background :)

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,82 +1,93 @@
-import React, { useContext } from 'react';
-import { withStyles } from '@material-ui/core/styles';
+import React, { useContext } from 'react'
+import { withStyles } from '@material-ui/core/styles'
 
-import Task from './Task';
-import Header from './Header';
-import NewTaskFab from './NewTaskFab';
-import NewTask from './NewTask';
-import EditTask from './EditTask';
-import NotificationSnack from './NotificationSnack';
-import WeeklySideBar from './WeeklyTasks/WeeklySideBar';
+import Task from './Task'
+import Header from './Header'
+import NewTaskFab from './NewTaskFab'
+import NewTask from './NewTask'
+import EditTask from './EditTask'
+import NotificationSnack from './NotificationSnack'
+import WeeklySideBar from './WeeklyTasks/WeeklySideBar'
 
-import { StoreContext } from '../context/store';
-import { LoadMondayTasks, LoadTasks, LoadTuesdayTasks, LoadWednesdayTasks, LoadFridayTasks, LoadThursdayTasks } from '../hooks/useDb';
+import { StoreContext } from '../context/store'
+import {
+  LoadMondayTasks,
+  LoadTasks,
+  LoadTuesdayTasks,
+  LoadWednesdayTasks,
+  LoadFridayTasks,
+  LoadThursdayTasks
+} from '../hooks/useDb'
 import { CSSTransitionGroup } from 'react-transition-group' // ES6
 
-import './../App.css';
-
+import './../App.css'
 
 const styles = () => ({
-    root: {
-        display: 'flex',
-        flex: 1,
-    },
-    wrapper: {
-        /* layout */
-        width: '100%',
-        height: 'calc(100vh - 62px)',
-        position: 'fixed',
-        display: 'flex',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        overflowY: 'auto',
-    },
-    cardContainer: {
-        flex: 1,
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'center',
-        alignItems: 'baseline',
-        overflowY: 'auto'
-    }
+  root: {
+    display: 'flex',
+    flex: 1
+  },
+  wrapper: {
+    /* layout */
+    width: '100%',
+    height: 'calc(100vh - 62px)',
+    position: 'fixed',
+    display: 'flex',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    overflowY: 'auto',
+    backgroundImage: 'url(https://source.unsplash.com/random/2560x1600/?night)'
+  },
+  cardContainer: {
+    flex: 1,
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignItems: 'baseline',
+    overflowY: 'auto'
+  }
 })
 
-
 const Home = ({ classes }) => {
-    const { state } = useContext(StoreContext)
-    LoadTasks()
-    LoadMondayTasks()
-    LoadTuesdayTasks()
-    LoadWednesdayTasks()
-    LoadThursdayTasks()
-    LoadFridayTasks()
+  const { state } = useContext(StoreContext)
+  LoadTasks()
+  LoadMondayTasks()
+  LoadTuesdayTasks()
+  LoadWednesdayTasks()
+  LoadThursdayTasks()
+  LoadFridayTasks()
 
-    return (
-        <div className="App">
-            <Header />
-            <div className={classes.wrapper}>
-                {state.loggedIn &&
-                    <>
-                        <WeeklySideBar />
-                        <CSSTransitionGroup
-                            className={classes.cardContainer}
-                            transitionName="simple"
-                            transitionEnterTimeout={500}
-                            transitionLeaveTimeout={300}>
-                            {state.showCompleted === false ? state.toDos.map(todo => <Task todo={todo} userUid={state.user.uid} key={todo.uid} />)
-                                : state.completedToDos.map(todo => <Task todo={todo} userUid={state.user.uid} key={todo.uid} />)
-                            }
-                        </CSSTransitionGroup>
-                    </>}
-            </div>
-            <EditTask />
-            <NewTask />
-            <NewTaskFab />
-            <NotificationSnack />
-        </div>
-    );
+  return (
+    <div className='App'>
+      <Header />
+      <div className={classes.wrapper}>
+        {state.loggedIn && (
+          <>
+            <WeeklySideBar />
+            <CSSTransitionGroup
+              className={classes.cardContainer}
+              transitionName='simple'
+              transitionEnterTimeout={500}
+              transitionLeaveTimeout={300}
+            >
+              {state.showCompleted === false
+                ? state.toDos.map(todo => (
+                    <Task todo={todo} userUid={state.user.uid} key={todo.uid} />
+                  ))
+                : state.completedToDos.map(todo => (
+                    <Task todo={todo} userUid={state.user.uid} key={todo.uid} />
+                  ))}
+            </CSSTransitionGroup>
+          </>
+        )}
+      </div>
+      <EditTask />
+      <NewTask />
+      <NewTaskFab />
+      <NotificationSnack />
+    </div>
+  )
 }
 
-
-export default withStyles(styles)(Home);
+export default withStyles(styles)(Home)
--- src/components/WeeklyTasks/WeeklySideBar.js
+++ src/components/WeeklyTasks/WeeklySideBar.js
@@ -4,38 +4,44 @@ import { StoreContext } from '../../context/store'
 
 import WeekDay from './WeekDay'
 
-
 const styles = () => ({
-    root: {
-        width: '14rem',
-        maxWidth: '14rem',
-        direction: 'rtl',
-        boxShadow: '0 2px 2px 0 rgba(0,0,0,.14), 0 3px 1px -2px rgba(0,0,0,.2), 0 1px 5px 0 rgba(0,0,0,.12)',
-    },
-    header: {
-        backgroundColor: '#0288d1 !important',
-        alignItems: 'center',
-        color: '#FFF !important',
-        boxShadow: '0 2px 2px 0 rgba(0,0,0,.14), 0 3px 1px -2px rgba(0,0,0,.2), 0 1px 5px 0 rgba(0,0,0,.12)',
-        height: '30px',
-        userSelect: 'none',
-    },
-    container: {
-        height: 'calc(100vh - 90px)',
-        overflowY: 'auto',
-    }
+  root: {
+    width: '14rem',
+    maxWidth: '14rem',
+    direction: 'rtl',
+    boxShadow:
+      '0 2px 2px 0 rgba(0,0,0,.14), 0 3px 1px -2px rgba(0,0,0,.2), 0 1px 5px 0 rgba(0,0,0,.12)'
+  },
+  header: {
+    backgroundColor: '#0288d1 !important',
+    alignItems: 'center',
+    color: '#FFF !important',
+    boxShadow:
+      '0 2px 2px 0 rgba(0,0,0,.14), 0 3px 1px -2px rgba(0,0,0,.2), 0 1px 5px 0 rgba(0,0,0,.12)',
+    height: '30px',
+    userSelect: 'none'
+  },
+  container: {
+    height: 'calc(100vh - 90px)',
+    overflowY: 'auto',
+    backgroundColor: 'white'
+  }
 })
 
 const WeeklySideBar = ({ classes }) => {
-    const { state } = useContext(StoreContext);
-    
-    return (
-        <div className={classes.root}>
-            <div className={classes.header}>Weekly recurrent activities</div>
-            <div className={classes.container}> {Object.entries(state.weeklyTasks).map(day => <WeekDay key={day[0]} day={day[0]} tasks={day[1]} />)} </div>
-        </div>
-    )
+  const { state } = useContext(StoreContext)
 
+  return (
+    <div className={classes.root}>
+      <div className={classes.header}>Weekly recurrent activities</div>
+      <div className={classes.container}>
+        {' '}
+        {Object.entries(state.weeklyTasks).map(day => (
+          <WeekDay key={day[0]} day={day[0]} tasks={day[1]} />
+        ))}{' '}
+      </div>
+    </div>
+  )
 }
 
 export default withStyles(styles)(WeeklySideBar)
